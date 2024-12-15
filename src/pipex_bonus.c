5bbff90c3a5325f4a5edab909b0e0422448742c9
aabdulmecitz <ahmetabdulmecid@gmail.com>
2024-12-15 03:55:15 +0300

    commit

--- src/pipex_bonus.c
+++ src/pipex_bonus.c
@@ -6,18 +6,18 @@
 /*   By: aabdulmecitz <aabdulmecitz@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/07 23:17:11 by aabdulmecit       #+#    #+#             */
-/*   Updated: 2024/12/15 03:33:59 by aabdulmecit      ###   ########.fr       */
+/*   Updated: 2024/12/15 03:55:02 by aabdulmecit      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../lib/libft/libft.h"
 #include "./pipex_bonus.h"
 
-void child_process (char *argv, char **envp)
+void child_process(char *argv, char **envp)
 {
     int fd[2];
     pid_t pid;
-    
+
     if (pipe(fd) == -1)
         error_msg("Pipe error!");
     pid = fork();
@@ -27,27 +27,31 @@ void child_process (char *argv, char **envp)
     {
         close(fd[0]);
         dup2(fd[1], STDOUT_FILENO);
+        close(fd[1]);
         execute(argv, envp);
     }
-    else if (pid != 0)    
+    else
     {
-        waitpid(-1, NULL, 0);
+        waitpid(pid, NULL, 0);
         close(fd[1]);
         dup2(fd[0], STDIN_FILENO);
-    }    
+        close(fd[0]);
+    }
 }
 
-void    here_doc(char **argv)
+void here_doc(char **argv)
 {
     int fd;
     char *line;
-    
+
     fd = open("./.here_doc", O_WRONLY | O_CREAT | O_APPEND, 0777);
+    if (fd == -1)
+        error_msg("here_doc temp file error!");
     while (1)
     {
         line = get_next_line(0);
         if (!line)
-            break;        
+            break;
         if (ft_strncmp(line, argv[2], ft_strlen(argv[2])) == 0)
         {
             free(line);
@@ -58,7 +62,10 @@ void    here_doc(char **argv)
     }
     close(fd);
     fd = open("./.here_doc", O_RDONLY);
-    dup2(fd, STDIN_FILENO);    
+    if (fd == -1)
+        error_msg("here_doc temp file read error!");
+    dup2(fd, STDIN_FILENO);
+    close(fd);
 }
 
 int main(int argc, char **argv, char **envp)
@@ -66,29 +73,26 @@ int main(int argc, char **argv, char **envp)
     int arg_index;
     int in_file;
     int out_file;
-    
-    arg_index = 0;
+
     if (argc < 5)
         args_error();
     if (ft_strncmp(argv[1], "here_doc", 8) == 0)
     {
         arg_index = 3;
-        out_file = open(argv[argc - 1], O_RDONLY | O_APPEND | O_CREAT, 0777);
+        out_file = open(argv[argc - 1], O_WRONLY | O_CREAT | O_APPEND, 0777);
         here_doc(argv);
     }
     else
     {
         arg_index = 2;
-        out_file = open(argv[argc - 1], O_WRONLY | O_TRUNC | O_CREAT, 0777);
         in_file = open(argv[1], O_RDONLY);
-        dup2(out_file, STDIN_FILENO);
+        dup2(in_file, STDIN_FILENO);
+        close(in_file);
+        out_file = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0777);
     }
     while (arg_index < argc - 2)
-    {
-        child_process(argv[arg_index], envp);
-        arg_index++;
-    }
+        child_process(argv[arg_index++], envp);
     dup2(out_file, STDOUT_FILENO);
-    execute(argv[arg_index - 2], envp);
-    return (0);
+    close(out_file);
+    return (execute(argv[arg_index], envp), 0);
 }
